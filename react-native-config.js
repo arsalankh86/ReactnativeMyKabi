ceca70f45082ccd2dfc362e68123997ca34c882f
arsalankh86 <softwares.coders@gmail.com>
2020-02-22 21:40:39 +0500

    Commit all files

---
+++ react-native-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  dependencies: {
+    'react-native.config': {
+      platforms: {
+        android: null, // disable Android platform, other platforms will still autolink if provided
+      },
+    },
+  },
