797b1594d0c5575dd11d5001a3fdff6dc3c75bd4
arsalankh86 <softwares.coders@gmail.com>
2020-02-22 22:17:56 +0500

    Correction for Mobile Platform

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 import { Image } from 'react-native';
 
 export default function App() {
@@ -10,18 +10,12 @@ export default function App() {
 
     <View style={styles.container}>
       <TextInput placeholder="Send message to Bro" style={{borderColor:'black' }}></TextInput>
-      <button>Send</button>
       <Image style={{ width:250, height:150}}
        source = {require('./assets/bro1.png')} />
-      <Text>Hey Bro 1234</Text>
+      
     </View>
   );
 }
-
-
-const ImagesExample = () => (
-  <Image source = {require('D:/React Projects/mykabi/assets/bro1.png')} />
-)
 
 const styles = StyleSheet.create({
   container: {
